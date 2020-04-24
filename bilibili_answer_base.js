8485aabe3354339e9acee91a9fc7840017c24735
xiongwn <21278854+xiongwn@users.noreply.github.com>
2020-04-24 12:17:26 +0800

    Update bilibili_answer_base.js
    b站自作聪明2020答题系统改版，但是前40道必须答对，怎么改版都很容易让我找到规律，最简单的就是穷举

--- bilibili_answer_base.js
+++ bilibili_answer_base.js
@@ -1,14 +1,22 @@
-for (let i = 0; i < 4; i++) {
-	setTimeout(() => {
-		for (let j = 0; j < 40; j++) {
-		let dom = $(`.answer-list .exam-list:nth-of-type(${j+1}) .key-list li:nth-of-type(${i+1})`)
-		if (i === 0) {
-			dom.trigger('click')
-		}
-		else if ($(`.answer-list .exam-list:nth-of-type(${j+1}) .key-list li:nth-of-type(${i})`).hasClass('error')) {
-			dom.trigger('click')
-		}
-	}
-	$('.btn-default, .btn-width').trigger('click')
-	}, i*3000)
-}
+// for (let i = 0; i < 4; i++) {
+// 	setTimeout(() => {
+// 		for (let j = 0; j < 40; j++) {
+// 		let dom = $(`.answer-list .exam-list:nth-of-type(${j+1}) .key-list li:nth-of-type(${i+1})`)
+// 		if (i === 0) {
+// 			dom.trigger('click')
+// 		}
+// 		else if ($(`.answer-list .exam-list:nth-of-type(${j+1}) .key-list li:nth-of-type(${i})`).hasClass('error')) {
+// 			dom.trigger('click')
+// 		}
+// 	}
+// 	$('.btn-default, .btn-width').trigger('click')
+// 	}, i*3000)
+// }
+//2018年版本，已作废
+
+index=0
+setInterval(()=>{
+	$(".answer-outer").eq(index).trigger("click")
+	index=index===3?0:index+1
+},1000)
+//2020年最新
